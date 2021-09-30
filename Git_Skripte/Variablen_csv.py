6ab169fba55161fca89838f63c22e23324322243
BlueberryDuck <82870811+BlueberryDuck@users.noreply.github.com>
2021-09-30 15:39:34 +0200

    Update aktualisiere_variablen_csv.yml

--- Git_Skripte/Variablen_csv.py
+++ Git_Skripte/Variablen_csv.py
@@ -1,8 +1,8 @@
 import os,re,csv
 directory_path = os.getcwd()
-PATH=f'{directory_path}\Dokumentation\Variablen'
+PATH=f'{directory_path}/Dokumentation/Variablen'
 
-file=open(f'{PATH}\Variablenübersicht.txt','r')
+file=open(f'{PATH}/Variablenübersicht.txt','r')
 data=file.read().split('\n')[1:-1]
 structured_variables=[]
 for line in data:
@@ -11,7 +11,7 @@ for line in data:
 details=structured_variables[0]
 rows=structured_variables[1:]
 
-with open(f'{PATH}\Variablenübersicht.csv', 'w') as file:
+with open(f'{PATH}/Variablenübersicht.csv', 'w') as file:
     write=csv.writer(file)
     write.writerow(details)
     write.writerows(rows)
