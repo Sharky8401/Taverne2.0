2d6e5edafb9f9f01e2fa4259663bbbda56dcdc5d
Sharky8401 <91247680+Sharky8401@users.noreply.github.com>
2021-10-01 13:04:20 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Person
+{
+    private String CName { get; set; }
+    private int Level { get; set; }
+    private List<Waffe> Waffen; //Liste an Waffen
+    private int aktuelleWaffe { get; set; } //Index der Waffe in der Liste der Waffen
+    private int DefR { get; set; } //Rüstungswerte (R = (Körper-)Rüstung,
+    private int DefH { get; set; } //H = Helm
+    private int DefS { get; set; } //S = Schild
+
+    public void erhoeheLevel()
+    {
+        Level++;
+    }
+
+    public void reduziereLevel()
+    {
+        Level--;
+    }
+
+    public void waffeHinzufuegen(Waffe pWaffe)
+    {
+        Waffen.Add(pWaffe);
+    }
+
+    public void waffeEntfernen(int pIndex)
+    {
+        Waffen.Remove(Waffen[pIndex]);
+    }
+
+    public void waffeSetzen(int pIndex)
+    {
+        aktuelleWaffe = pIndex;
+    }
+
+    public List<Waffe> getWaffenliste()
+    {
+        return Waffen;
+    }
+}
