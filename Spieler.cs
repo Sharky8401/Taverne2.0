57bb8449b6ba4fb59a65361165ff1bf978ca1c51
Sharky8401 <91247680+Sharky8401@users.noreply.github.com>
2021-10-01 14:56:28 +0200

    Add files via upload

---
+++ Spieler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Spieler : Person
+{
+    public int Staerkebonus { get; set; }
+
+    //Sachen, die noch nicht da sind (Charakter hat keinen Helm oder so), können einfach ausgelassen werden, also (...Rüstung, ,Schild)
+    public Spieler(String pCName, int pLevel, Waffe pWaffe, int pDefR, int pDefH, int pDefS)
+    {
+        CName = pCName;
+        Level = pLevel;
+        Waffen.Add(pWaffe);
+        DefR = pDefR;
+        DefH = pDefH;
+        DefS = pDefS;
+    }
+}
