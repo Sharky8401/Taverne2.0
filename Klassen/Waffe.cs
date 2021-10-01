3160b46d6f6192098ef70711e7c01937e85fa40e
Sharky8401 <91247680+Sharky8401@users.noreply.github.com>
2021-10-01 13:21:18 +0200

    Update Waffe.cs

--- Klassen/Waffe.cs
+++ Klassen/Waffe.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 
+//Waffe ist eine Klasse für Angriffsmöglichkeiten eines Spielers - also hauptsächlich Waffen, aber auch Fähigkeiten, Zauber usw.
+
 public class Waffe
 {
-    public String WName;
-    public int WSchaden;
-    public bool Schadensart;
+    public String WName; //Name der Waffe
+    public int WSchaden; //(Grund-)Schaden der Waffe
+    public bool Schadensart; //Normaler Schaden (True - Ruestung zählt) und RuA (False - Schaden ist Ruestungsunabhaengig)
     public bool Staerkeeinfluss;
 
     public Waffe(String pWName, int pWSchaden, bool pSchadensart, bool pStaerkeeinfluss)
