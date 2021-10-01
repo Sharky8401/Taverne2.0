df92e718947fcc2db30dd359584d208dead662df
Sharky8401 <91247680+Sharky8401@users.noreply.github.com>
2021-10-01 12:09:28 +0200

    Add files via upload

---
+++ Waffe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Waffe
+{
+    public String WName;
+    public int WSchaden;
+    public bool Schadensart;
+    public bool Staerkeeinfluss;
+
+    public Waffe(String pWName, int pWSchaden, bool pSchadensart, bool pStaerkeeinfluss)
+    {
+        WName = pWName;
+        WSchaden = pWSchaden;
+        Schadensart = pSchadensart;
+        Staerkeeinfluss = pStaerkeeinfluss;
+    }
+}
